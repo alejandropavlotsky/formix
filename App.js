8c36cb8e62fc9f7faaa2d6c7e46b1d7aed11a6a8
Alejandro Murawczik <alejandro.murawczik.p@gmail.com>
2021-01-07 18:02:06 +0100

    VALIDATE form

--- App.js
+++ App.js
@@ -1,11 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { useFormik } from 'formik';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+
+const validate = values => {
+  const errors = {}
+  if(!values.email){
+    errors.email = 'Requerido'
+  }else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)){
+    errors.email = 'Introduce un email valido'
+  }
+  return errors
+}
 
 export default function App() {
+  const formik = useFormik({
+    initialValues: {
+      email: 'lala@lala.com',
+    },
+    validate,
+    onSubmit: x => console.warn(x)
+  })
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Correo electrónico</Text>
+      <TextInput 
+        onChangeText={formik.handleChange('email')}
+        value={formik.values.email}
+      />
+      {formik.errors.email ? <Text>{formik.errors.email}</Text> : null}
+      <Button 
+        title='lala'
+        onPress={formik.handleSubmit}
+      />
       <StatusBar style="auto" />
     </View>
   );
